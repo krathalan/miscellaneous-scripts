f9a30f9b38f682a9a4d0189dac8f1ad334c18650
Hunter Peavey <srht@krathalan.net>
2020-07-11 11:33:35 -0700

    Update system_maintenance.sh root partition info for non-luks/lvm root partitions

--- system_maintenance.sh
+++ system_maintenance.sh
@@ -18,6 +18,29 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# This file incorporates work from https://github.com/dylanaraps/pash,
+# covered by the following copyright and permission notice:
+#
+#     Copyright (c) 2016-2019, Dylan Araps
+#
+#     Permission is hereby granted, free of charge, to any person obtaining a copy
+#     of this software and associated documentation files (the "Software"), to deal
+#     in the Software without restriction, including without limitation the rights
+#     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#     copies of the Software, and to permit persons to whom the Software is
+#     furnished to do so, subject to the following conditions:
+#
+#     The above copyright notice and this permission notice shall be included in all
+#     copies or substantial portions of the Software.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#     SOFTWARE.
 
 # -----------------------------------------
 # -------------- Guidelines ---------------
@@ -74,6 +97,24 @@ exit_script_on_failure()
   printf "Exiting %s Bash script.\n" "${SCRIPT_NAME}" >&2
 
   exit 1
+}
+
+#######################################
+# This is a simple wrapper around a case statement to allow
+# for simple string comparisons against globs.
+# Copyright (C) 2016-2019 Dylan Araps
+# Globals:
+#   none
+# Arguments:
+#   $1: string to check against glob
+#   $2: glob
+# Returns:
+#   true or false
+#######################################
+glob() {
+  # Disable this warning as it is the intended behavior.
+  # shellcheck disable=2254
+  case $1 in $2) return 0; esac; return 1
 }
 
 #######################################
@@ -215,6 +256,10 @@ printf "\n%s. Printing root disk usage...\n" "${stepWithColor}"
 # Get root partition info
 rootPartitionInfo="$(df -h | grep -G ".*/$")"
 
-printf "%s/%s, %s full\n" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f6)" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f3)" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f10)"
+if glob "${rootPartitionInfo}" "/dev/mapper*"; then
+  printf "%s/%s, %s full\n" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f6)" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f3)" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f10)"
+else
+  printf "%s/%s, %s full\n" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f12)" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f10)" "$(printf "%s" "${rootPartitionInfo}" | cut -d' ' -f17)"
+fi
 
 complete_step
